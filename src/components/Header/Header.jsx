070431bae77cc4c10dc3eef9b3607b177a85d96c
yuriy-kulakovskyi <yurakulak42@gmail.com>
2023-06-16 16:44:39 +0300

    adaptability for the page is done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,33 @@
-import React from 'react'
+import React from 'react';
+
+// styles
+import './Header.css';
 
 export default function Header() {
   return (
     <div>
     <header>
         <div>
-            <span className='logo'>House Staff</span>
+          <span className='logo'>House Staff</span>
         </div>
         <nav>
-       
-                <ul>
-                    <li>Головна</li>
-                    <li>Товари</li>
-                    <li>Акції</li>
-                    <li>Правила</li>
-                    <li>Про нас</li>
-                    <li>Контакти</li>
-                    <li>Кабінет</li>
-                </ul>
+          <ul>
+              <li>Головна</li>
+              <li>Товари</li>
+              <li>Акції</li>
+              <li>Правила</li>
+              <li>Про нас</li>
+              <li>Контакти</li>
+              <li>Кабінет</li>
+          </ul>
         </nav>
         
+        {/* burger button */}
+        <div className="burger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
     </header>
     <div className="presentation">
         <h1>Кращі товари для дому</h1>
